762ab66013a3c93c2cc8684c1ec775d8c220fb15
ylnhari <45874226+ylnhari@users.noreply.github.com>
2022-06-13 16:26:05 +0530

    added function to communicate with a slack channel.

--- utils.py
+++ utils.py
@@ -35,3 +35,52 @@ def progress_bar_for_your_loop():
         boolean value
     """
     return string_value.lower() in ("yes", "true", "t", "1")
+
+def send_message_to_slack():
+  """
+  From your workflows you may need to post messages to slack about 
+          notifying about the run status of your programs, 
+          sharing results/records created etc.
+  This Function helps you in acheiving it.     
+  """
+  import sys
+  import logging
+  import json
+  import requests
+  
+  # webhook_url (str) : a url of the incoming webhook of the slack pointing towards a channel.
+  # see this article for webhook creation -> https://slack.com/intl/en-in/help/articles/115005265063-Incoming-webhooks-for-Slack
+  webhook_url =  "https://hooks.slack.com/services/*******"
+
+  # message you need to send
+  message = "what you want to display in slack"
+  
+  # you can create rich, interactive UI based messages , pictures etc 
+  # use this to create a the body for the above mentioned types of messages -> https://app.slack.com/block-kit-builder
+  # prepare body for a post request
+  body = {
+    "attachments": [
+      {
+        "blocks": [
+          {
+            "text": {
+              "text": f"{message}",
+              "type": "mrkdwn"
+            },
+            "type": "section"
+          }
+        ],
+        "color": "#9733EE"
+      }
+    ]
+  }
+  # prepare headers
+  byte_length = str(sys.getsizeof(body))
+  headers = {'Content-Type': "application/json",
+             'Content-Length': byte_length}
+  # send a post request
+  response = requests.post(webhook_url, data=json.dumps(body), headers=headers)
+
+  if response.status_code != 200: # if message is not successfully sent
+      logging.error("Log you error")
+      # perform necessary error handling
