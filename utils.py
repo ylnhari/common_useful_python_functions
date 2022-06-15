6b654a6a467d8fb18e9d20d2bbcb2730366e6d39
ylnhari <ylnharimailme@gmail.com>
2022-06-15 19:01:00 +0530

    added functions to fetch address coordinates(Latitude, Longitude) using Google Maps API.

--- utils.py
+++ utils.py
@@ -84,3 +84,70 @@ def send_message_to_slack():
   if response.status_code != 200: # if message is not successfully sent
       logging.error("Log you error")
       # perform necessary error handling
+
+      
+def get_latitude_longitude_from_address(address):
+  """This Function helps to Fetch Latitide and Longitude values for a given address from the google Maps API's
+  Inorder to use this function we need a Google Maps API account setup and an Access Key.
+  More About the setup can be found here -> https://developers.google.com/maps/get-started
+  Parameters
+  ----------
+  address : (str) address of the location.
+  Returns
+  -------
+  tuple : latitude(str), longitude(str)
+  """
+  import googlemaps
+  gmaps = googlemaps.Client(key='Add your Key')
+  geocode_result = gmaps.geocode(address)
+  latitude  = geocode_result[0]['geometry']['location']['lat']
+  longitude = geocode_result[0]['geometry']['location']['lng']
+  return latitude, longitude
+
+
+def get_latitude_longitude_from_address_with_exception_handling(addresses):
+  """Typically we may need to extract coordinates for multiple locations at the same time so there
+  are high chances of failure while calling Google Map GeoDecode API.So this function helps us 
+  to process addresses sequencially and save the results in a file either in case of execptions or successfull run.
+  This Function also helps to resume the activity after clearing the exception by fetching the saved
+  co-ordinates file and processing only those addresses which are not already processed.
+  Parameters
+  ----------
+  addresses : (list) a python list of address(str) of the location.
+  Functions called
+  ----------------
+  get_latitude_longitude_from_address
+  """
+  from pathlib import Path
+  import pandas as pd
+  from tqdm import tqdm
+  name_of_the_file_to_save = "your_file_name.csv"
+  if run_this_cell:
+      # Fetch any existing processed records
+      if Path(name_of_the_file_to_save).exists():
+          # read already fetched files
+          location_coordinates_df = pd.read_csv(name_of_the_file_to_save)
+          # create dataframe of fetched locations
+          locations_already_processed = location_coordinates_df["Name"].to_list()
+      else: # if there are no  pre fetched locations
+          # create empty data frame to fetch locations
+          locations_already_processed = []
+          location_coordinates_df = pd.DataFrame(columns = ['Name', 'latitude', 'longitude'])
+
+      # Fetch Location Coordinates
+      try:
+          for location_address in tqdm(addresses): # for every location
+              if location_address not in locations_already_processed: # if it is not pre fetched
+                  latitude, longitude = get_latitude_logitude(location_address) # get coordinates
+                  # append coordinates to dataframe
+                  location_coordinates_df = location_coordinates_df.append({'Name' : location_address, 'latitude' : latitude, 'longitude' : longitude},
+                                                                          ignore_index = True)
+                  locations_already_processed.append(location_address)
+          print("All addresses fetched successfully !!!")
+      except Exception as e: # if error occured while calling Google API
+          print(f"Error Occured While Processing address {location_address}")
+          print(f"Error : {e}")
+      finally:
+          # Finally Save File
+          location_coordinates_df.to_csv(name_of_the_file_to_save, index=False)
+          print(f"Fetched Coordinates saved to file {name_of_the_file_to_save}")
