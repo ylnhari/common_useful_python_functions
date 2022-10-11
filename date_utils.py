94a967c0529cb62c399ef5d22fda1443cf4d3d19
ylnhari <ylnharimailme@gmail.com>
2022-10-11 11:10:12 +0530

    Update date_utils.py

--- date_utils.py
+++ date_utils.py
@@ -13,3 +13,27 @@ def get_time_difference_from_a_start_date(pandas_series, start_date):
     """
     return (pandas_series - start_date).apply(lambda x: int(x.total_seconds() / 60))
     
+    
+def convert_pandas_col_to_datetime (df, col_name):
+    """
+    Parameters
+        df (pandas.DataFrame object): dataframe with date column in it
+        col_name (str): Name of the column.
+    return
+        df (pandas.DataFrame object)
+    """
+    df.loc[:,col_name] = pd.to_datetime(df[col_name])
+    return df
+
+
+def add_minutes_to_pandas_col(df, col_name, no_of_min):
+    """Add/Subtract minutes/hours/seconds from a datetime column
+    Parameters
+        df (pandas.DataFrame object): dataframe with date column in it
+        col_name (str): Name of the column.
+    return
+        df (pandas.DataFrame object)
+    """
+    minutes = pd.Timedelta (no_of_min, unit 'minutes') # similarly we can do it for seconds, hours (subraction). 
+    df[col_name] = (df[col_name]) - minutes
+    return df
