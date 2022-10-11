bdf94c0840e34b33595b73b70c0b124b10ce7e07
ylnhari <ylnharimailme@gmail.com>
2022-10-11 10:51:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,3 @@
-def list_all_files_with_sepcific_extension_under_a_root_directory():
-  """List all files under a root directory belonging to a specific Extension and save them as a list.
-  """
-  import glob
-  root_path = r"C:\Users\laxmi.n.yelesetty\Downloads\transportplandata"
-  file_extension = ".json"
-  json_files = glob.glob(f"{root_path}\**\*{file_extension}", recursive=True)
-  print(f"There are a total of {len(json_files)} Files in under the root path")
-
-  
 def progress_bar_for_your_loop():
   """Adding Progress BAr to you loop.
   
