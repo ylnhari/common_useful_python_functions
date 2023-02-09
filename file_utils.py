4a1d722dee0b2dc635f1f8d4e7840055bc2ee17d
ylnhari <ylnharimailme@gmail.com>
2023-02-09 15:21:38 +0530

    Update file_utils.py
    added file list extraction function under a directory.

--- file_utils.py
+++ file_utils.py
@@ -9,6 +9,13 @@ def list_all_files_with_sepcific_extension_under_a_root_directory():
   json_files = glob.glob(f"{root_path}\**\*{file_extension}", recursive=True)
   print(f"There are a total of {len(json_files)} Files in under the root path")
 
+def list_only_file_name_and_path_under_a_path(path: pathlib.Path) -> dict:
+  """Under a Directory we will iterate through all directories, files and get all file 
+  names and their full paths as a dictionary where key is file name and value as file path.
+  """
+  files = {x.name: x for x inpath.iterdir() if x.is_file()}
+  return files
+
 def save_xls(dict_df, path):
     """Save a dictionary of dataframes to an excel file, with each dataframe as a separate page.
     Parameters
